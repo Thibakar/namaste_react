b029671d5c5840a0fccfc78cc7cf82dd32d68d73
unknown <thiba800@hotmail.com>
2024-03-17 15:11:37 +0530

    02-creating react components with JSX

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
-import React from "react"
-import  ReactDOM  from "react-dom/client"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./App.css";
 
-const Parent = React.createElement('p', { id: 'parent' },  [
-    React.createElement('div', {}, 'i am from parent'),
+//React CreateElemet
+const title = React.createElement("h2", {}, "Hello ....!");
 
-    React.createElement('p', { id: 'child' }, [
-        React.createElement('div', {}, 'i am from children'),
-    ])
+//React Function
+const jsFunction =()=>(console.log('javascript Function'))
+const number = 1000;
 
-]);
+//React Component 
+const Heading = () => <h2>Namaste React using JSX</h2>;
+const HeadingComponent = () => (
+  <div className="container">
+    {title}
+    {jsFunction()}
+    <Heading />
+    <h2>{number}</h2>
+    <h2> React Functional Component using JSX </h2>
+    <h2>{100 + 100}</h2>
+  </div>
+);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(Parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
