c0e118ea687a26a06965633265db6590fb9a7a99
unknown <thiba800@hotmail.com>
2024-04-07 21:09:16 +0530

    test

---
+++ src/components/Error/Error.jsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const NoDataFound = ({}) => {
+  return (
+    <div className="NoDataFound-container">
+      <h4>OOPs....!!!!!</h4>
+      <h5>Something Went Wrong........!!!!!!!!!!!!!!!!!!!!!!!</h5>
+    </div>
+  );
+};
+export default NoDataFound;
